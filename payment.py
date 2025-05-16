8408f2cf67d64853d04e83dfc2463ae7fed0a83a
lubnaNoor0210 <167392315+lubnaNoor0210@users.noreply.github.com>
2025-05-16 16:42:38 +0500

    Update payment.py

--- payment.py
+++ payment.py
@@ -1,5 +1,4 @@
 import stripe
-import streamlit as st
 
 class PaymentProcessor:
     def __init__(self, secret_key):
@@ -7,14 +6,7 @@ class PaymentProcessor:
 
     def create_checkout_session(self, amount_in_dollars):
         try:
-            YOUR_NGROK_URL = "https://demo-quran-guide.streamlit.app" 
-            user = st.session_state.get("user", {})
-            id_token = user.get("idToken")
-            user_id = user.get("localId")
-
-            if not id_token or not user_id:
-                st.error("🔐 You must be logged in to make a payment.")
-                return None
+            YOUR_NGROK_URL = "https://demo-quran-guide.streamlit.app"
 
             session = stripe.checkout.Session.create(
                 payment_method_types=['card'],
@@ -29,7 +21,7 @@ class PaymentProcessor:
                     'quantity': 1,
                 }],
                 mode='payment',
-                success_url = f"{YOUR_NGROK_URL}/?status=success&tab=donate&token={id_token}&uid={user_id}",
+                success_url=f'{YOUR_NGROK_URL}/?status=success',
                 cancel_url=f'{YOUR_NGROK_URL}/?status=cancel',
                 customer_email="test@example.com",
                 billing_address_collection='auto',
